27c586d7f44e63f5f9604a99c386c5abf07aee36
virnaaguiaar <virnaaguiaar@gmail.com>
2026-05-28 13:40:03 -0300

    .

--- src/components/controle.jsx
+++ src/components/controle.jsx
@@ -31,63 +31,66 @@ function Controle() {
             
             <div className="flex flex-row mt-10 space-x-4">
                 {/* ESQUERDA */}
-                <div className="flex flex-col justify-center items-center">
-                    <button
-                        onMouseDown={() => handleStart(-5, 0, 'baixo')}
-                        onMouseUp={handleStop}
-                        onTouchStart={() => handleStart(-5, 0, 'baixo')}
-                        onTouchEnd={handleStop}
-                        onContextMenu={(e) => e.preventDefault()}
-                        className={`flex justify-center items-center w-16 h-16 transition-transform duration-100 ${
-                            activeDir === 'baixo' ? 'scale-110' : 'hover:scale-105'
-                        }`}
-                    >
-                        <img src={Baixo} alt="seta baixo" className="w-12 h-12" />
-                    </button>
-                </div>
-                
-                {/* CIMA/BAIXO */}
-                <div className="flex flex-col items-center space-y-4">
-                    <button
-                        onMouseDown={() => handleStart(0, 5, 'direita')}
-                        onMouseUp={handleStop}
-                        onTouchStart={() => handleStart(0, 5, 'direita')}
-                        onTouchEnd={handleStop}
-                        onContextMenu={(e) => e.preventDefault()}
-                        className={`flex justify-center items-center w-16 h-16 transition-transform duration-100 ${
-                            activeDir === 'direita' ? 'scale-110' : 'hover:scale-105'
-                        }`}
-                    >
-                        <img src={Direita} alt="seta direita" className="w-12 h-12" />
-                    </button>
-                    <button
-                        onMouseDown={() => handleStart(0, -5, 'esquerda')}
-                        onMouseUp={handleStop}
-                        onTouchStart={() => handleStart(0, -5, 'esquerda')}
-                        onTouchEnd={handleStop}
-                        onContextMenu={(e) => e.preventDefault()}
-                        className={`flex justify-center items-center w-16 h-16 transition-transform duration-100 ${
-                            activeDir === 'esquerda' ? 'scale-110' : 'hover:scale-105'
-                        }`}
-                    >
-                        <img src={Esquerda} alt="seta esquerda" className="w-12 h-12" />
-                    </button>
-                </div>
-                
-                {/* DIREITA */}
-                <div className="flex flex-col justify-center items-center">
-                    <button
-                        onMouseDown={() => handleStart(5, 0, 'cima')}
-                        onMouseUp={handleStop}
-                        onTouchStart={() => handleStart(5, 0, 'cima')}
-                        onTouchEnd={handleStop}
-                        onContextMenu={(e) => e.preventDefault()}
-                        className={`flex justify-center items-center w-16 h-16 pl-[10%] transition-transform duration-100 ${
-                            activeDir === 'cima' ? 'scale-110' : 'hover:scale-105'
-                        }`}
-                    >
-                        <img src={Cima} alt="seta cima" className="w-12 h-12" />
-                    </button>
+                <div className="flex flex-row mt-10 space-x-4">
+                    {/* ESQUERDA - Gira/Gira */}
+                    <div className="flex flex-col justify-center items-center">
+                        <button
+                            onMouseDown={() => handleStart(0, -5, 'esquerda')}
+                            onMouseUp={handleStop}
+                            onTouchStart={() => handleStart(0, -5, 'esquerda')}
+                            onTouchEnd={handleStop}
+                            onContextMenu={(e) => e.preventDefault()}
+                            className={`flex justify-center items-center w-16 h-16 transition-transform duration-100 ${
+                                activeDir === 'esquerda' ? 'scale-110' : 'hover:scale-105'
+                            }`}
+                        >
+                            <img src={Esquerda} alt="seta esquerda" className="w-12 h-12" />
+                        </button>
+                    </div>
+                    
+                    {/* CIMA/BAIXO - Movimento frente/trás */}
+                    <div className="flex flex-col items-center space-y-4">
+                        <button
+                            onMouseDown={() => handleStart(5, 0, 'cima')}      // Y positivo = frente
+                            onMouseUp={handleStop}
+                            onTouchStart={() => handleStart(5, 0, 'cima')}
+                            onTouchEnd={handleStop}
+                            onContextMenu={(e) => e.preventDefault()}
+                            className={`flex justify-center items-center w-16 h-16 transition-transform duration-100 ${
+                                activeDir === 'cima' ? 'scale-110' : 'hover:scale-105'
+                            }`}
+                        >
+                            <img src={Cima} alt="seta cima" className="w-12 h-12" />
+                        </button>
+                        <button
+                            onMouseDown={() => handleStart(-5, 0, 'baixo')}    // Y negativo = trás
+                            onMouseUp={handleStop}
+                            onTouchStart={() => handleStart(-5, 0, 'baixo')}
+                            onTouchEnd={handleStop}
+                            onContextMenu={(e) => e.preventDefault()}
+                            className={`flex justify-center items-center w-16 h-16 transition-transform duration-100 ${
+                                activeDir === 'baixo' ? 'scale-110' : 'hover:scale-105'
+                            }`}
+                        >
+                            <img src={Baixo} alt="seta baixo" className="w-12 h-12" />
+                        </button>
+                    </div>
+                    
+                    {/* DIREITA - Gira/Direita */}
+                    <div className="flex flex-col justify-center items-center">
+                        <button
+                            onMouseDown={() => handleStart(0, 5, 'direita')}
+                            onMouseUp={handleStop}
+                            onTouchStart={() => handleStart(0, 5, 'direita')}
+                            onTouchEnd={handleStop}
+                            onContextMenu={(e) => e.preventDefault()}
+                            className={`flex justify-center items-center w-16 h-16 pl-[10%] transition-transform duration-100 ${
+                                activeDir === 'direita' ? 'scale-110' : 'hover:scale-105'
+                            }`}
+                        >
+                            <img src={Direita} alt="seta direita" className="w-12 h-12" />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
